317127f3a641eb5d0ee6acd9e8cf6f88a3e085e3
Anna Krivova <ani94@mail.bg>
2015-06-13 03:23:22 +0300

    Header Carousel fixed

--- DroxicJuniorFE_test_v1.0/js/script.js
+++ DroxicJuniorFE_test_v1.0/js/script.js
@@ -17,39 +17,18 @@ google.maps.event.addDomListener(window, 'load', initialize);
 /*
 * Jquery Carousel
 */
-(function($) {
+$(document).ready(function() {
+    $('#carousel').roundabout({
+        childSelector:"img",
+        tilt:-2,
+        minOpacity: 3,
+        minScale: 0.4,
+        btnNext: ".right",
+        btnPrev: ".left"
 
-var $descriptions = $('#carousel-descriptions').children('li'),
-    $controls = $('#carousel-controls').find('span'),
-    $carousel = $('#carousel')
-        .roundabout({childSelector:"img", minOpacity:1, autoplay:true, autoplayDuration:5000, autoplayPauseOnHover:true })
-        .on('focus', 'img', function() {
-            var slideNum = $carousel.roundabout("getChildInFocus");
-
-            $descriptions.add($controls).removeClass('current');
-            $($descriptions.get(slideNum)).addClass('current');
-            $($controls.get(slideNum)).addClass('current');
-        });
-
-$controls.on('click dblclick', function() {
-    var slideNum = -1,
-        i = 0, len = $controls.length;
-
-    for (; i<len; i++) {
-        if (this === $controls.get(i)) {
-            slideNum = i;
-            break;
-        }
-    }
-
-    if (slideNum >= 0) {
-        $controls.removeClass('current');
-        $(this).addClass('current');
-        $carousel.roundabout('animateToChild', slideNum);
-    }
+    });
 });
 
-}(jQuery));
 /* /Jquery Carousel */
 
 /*
